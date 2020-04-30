8316ef0741913d8bd3aa6f65a55f60756ae8aa97
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-04-30 15:51:56 -0500

    Created list view of requests for a campaign.

--- pages/campaigns/[address]/requests/index.js
+++ pages/campaigns/[address]/requests/index.js
@@ -1,14 +1,34 @@
 import React from "react";
 import Link from "next/link";
+import { Button, Table } from "semantic-ui-react";
 import Layout from "../../../../components/Layout";
-import { Button } from "semantic-ui-react";
+import RequestRow from "../../../../components/RequestRow";
+import Campaign from "../../../../ethereum/campaign";
 
 export const getServerSideProps = async (ctx) => {
   const { address } = ctx.query;
-  return { props: { address } };
+  const campaign = Campaign(address);
+  const requestCount = await campaign.methods.getRequestsCount().call();
+
+  const requests = await Promise.all(
+    Array(parseInt(requestCount))
+      .fill()
+      .map((elm, idx) => {
+        return campaign.methods.requests(idx).call();
+      })
+  );
+  return { props: { address, requests, requestCount } };
 };
 
-const RequestIndex = ({ address }) => {
+const RequestIndex = ({ address, requests, requestCount }) => {
+  const { Header, HeaderCell, Body } = Table;
+
+  renderRows = () => {
+    return requests.map((request, idx) => (
+      <RequestRow key={idx} id={idx} request={request} address={address} />
+    ));
+  };
+
   return (
     <Layout>
       <h3>Request List</h3>
@@ -17,6 +37,20 @@ const RequestIndex = ({ address }) => {
           <Button primary>Add Request</Button>
         </a>
       </Link>
+
+      <Table>
+        <Header>
+          <HeaderCell>ID</HeaderCell>
+          <HeaderCell>Description</HeaderCell>
+          <HeaderCell>Amount</HeaderCell>
+          <HeaderCell>Recipient</HeaderCell>
+          <HeaderCell>Approval Count</HeaderCell>
+          <HeaderCell>Approve</HeaderCell>
+          <HeaderCell>Finalize</HeaderCell>
+        </Header>
+
+        <Body>{renderRows()}</Body>
+      </Table>
     </Layout>
   );
 };
