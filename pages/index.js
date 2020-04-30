51b78b772d563ee8c6fb49bad9a5c199f115e01f
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-04-30 08:00:32 -0500

    Added semantic ui react for styling.

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import { Card } from "semantic-ui-react";
 import factory from "../ethereum/factory";
 
 export const getStaticProps = async () => {
@@ -7,7 +8,25 @@ export const getStaticProps = async () => {
 };
 
 const CampaignIndex = ({ campaigns }) => {
-  return <h1>{campaigns}</h1>;
+  const renderCampaigns = () => {
+    const items = campaigns.map((address) => ({
+      header: address,
+      description: <a>View Campaign</a>,
+      fluid: true,
+    }));
+
+    return <Card.Group items={items} />;
+  };
+
+  return (
+    <div>
+      <link
+        rel="stylesheet"
+        href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css"
+      />
+      {renderCampaigns()}
+    </div>
+  );
 };
 
 export default CampaignIndex;
