10ce5153c559128dda9b59be3fbb2c2235287bd2
ethanblumenthal <ethan.blumenthal@gmail.com>
2020-04-30 20:09:31 -0500

    Fixed window metamask error.

--- ethereum/web3.js
+++ ethereum/web3.js
@@ -3,6 +3,7 @@ import Web3 from "web3";
 let web3;
 
 if (typeof window !== "undefined" && typeof window.web3 !== "undefined") {
+  window.ethereum.enable();
   web3 = new Web3(window.web3.currentProvider);
 } else {
   const provider = new Web3.providers.HttpProvider(
